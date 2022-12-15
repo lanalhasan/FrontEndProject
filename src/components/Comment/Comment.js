77bb61469fd1caf9fce25f24e8ec4e8bd10ae65f
lanalhasan <louailana4@gmail.com>
2022-12-15 12:58:37 +0300

    ...

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,20 +1,37 @@
-import dayjs from "dayjs"
-import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "./Comment.css";
 
-const SingleComment =({comments}) => {
-const Time = (date) => {
-    dayjs.extend (relativeTime)
-    return dayjs(date).fromNow()
-}
-return (
-    <>
-    <div id='commentBox'>
-    <img src={comments.user.avatar }id ='userImg'/>
-        <div id='userName'>{comments.user.name}</div>
-        <div id='sinceWhen'>{Time(comments.created_at)}</div>
-        <div id='commentContent'>{comments.content}</div>
+
+const SingleComment = ({ comments }) => {
+  const Time = (date) => {
+    dayjs.extend(relativeTime);
+    return dayjs(date).fromNow();
+  };
+  return (
+    <div className="completeComment d-flex mb-2">
+      <div id="commentBox">
+        <div className="commentBody">
+          <div>
+            <img
+              className="CommentImg"
+              src={comments.user.avatar}
+              id="userImg"
+            />
+          </div>
+          <div>
+            <div className="CommentImg" id="userName">
+              {comments.user.name}
+            </div>
+            <div className="mb-2" id="sinceWhen">
+              {Time(comments.created_at)}
+            </div>
+        </div>
+           
+          </div>
+          <div id="commentContent">{comments.content}</div>
+      </div>
     </div>
-    </>
-)
-}
-export default SingleComment
+  );
+};
+export default SingleComment;
