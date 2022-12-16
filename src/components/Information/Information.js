121b3cf824b1a821bef792083c041f1bb3b21ed9
lanalhasan <louailana4@gmail.com>
2022-12-16 14:50:47 +0300

    ..

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,7 +1,28 @@
-import SignUp from '../../pages/SignUp/SignUp'
+import { useContext, useState,useEffect } from 'react'
+import { AuthContext } from '../../Contexts/AuthContext'
 import './Information.css'
 
 const Information = () => {
+    const [userInfo, setUserInfo] = useState({})
+    const {token} = useContext(AuthContext)
+
+    const getUserInfo = async () => {
+        const res = await fetch(process.env.REACT_APP_API +`/users/me`,{
+            method: "get",
+            headers: {
+             "Content-Type": "application/json",
+              Authorization : `Bearer ${token}`
+            }
+          })
+          const json = await res.json();
+          if(json.success) setUserInfo(json.data)
+          console.log(userInfo)
+    } 
+
+    useEffect(()=>{
+        getUserInfo()
+    },[])
+
     return (
         <div className='info'>
             <div  className='prof navbar sticky-top bg-light w-100'><h4 className='m-0 pt-1 px-3'>Profile</h4></div>
@@ -10,15 +31,15 @@ const Information = () => {
                 <div className="alert alert-info" role="alert">My Information</div>
                 <div  className="form-field mb-3 user-avatar">
                     <label htmlFor="avatar" className="mx-auto my-2 d-block w-25">
-                    <img src="https://www.gravatar.com/avatar/1683ce7f573f1af1584ea5b33bbf0e19?s=200" className="d-block mx-auto rounded-circle w-100"/>
+                    <img src={userInfo.avatar} className="d-block mx-auto rounded-circle w-100"/>
                     </label>
                 </div>
                 <div>
                 <label htmlFor='name' className=' mb-2'>Name<h6 className='star'>*</h6></label>
-                        <input type='text' id='name' className='form-control mb-3' />
+                        <input type='text' id='name' className='form-control mb-3' value={userInfo.name} />
 
                         <label htmlFor='email' className='mb-2'>Ema il Address<h6 className='star'>*</h6></label>
-                        <input  type='email' id='email' className='form-control mb-3' />
+                        <input value ={userInfo.email}  type='email' id='email' className='form-control mb-3' />
 
                         <label htmlFor='password' className='mb-2 '>Password<h6 className='star'>*</h6></label>
                         <input  type='password' id='password' className='form-control mb-3' />
@@ -33,7 +54,21 @@ const Information = () => {
                 </div>
                 </div>
             </form>
-            <div class="mb-4 p-3"><div class="alert alert-info">My Posts</div><ul class="list-group"></ul></div>
+            <div className="mb-4 p-3">
+                <div class="alert alert-info">My Posts</div>
+            <ul className="list-group">
+                {userInfo?.posts?.map((item,i)=>(
+
+                     <li className='list-group-item d-flex align-items-center justify-content-between'>
+                     <span>{item.content}</span>
+                     <span><button class="btn btn-danger btn-sm">Delete</button></span>
+                     </li>
+
+
+                ))}
+               
+            </ul>
+            </div>
         </div>
     )
 }
